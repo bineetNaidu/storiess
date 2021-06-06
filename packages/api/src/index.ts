52901ac0060157efda90908bf53202aa04a86825
bineetNaidu <bineet.s.naidu@gmail.com>
2021-06-06 12:19:10 +0530

    🎉 add apollo-server-express w/ context

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,25 +1,51 @@
 import 'reflect-metadata';
 import dotenv from 'dotenv';
-import { ApolloServer } from 'apollo-server';
+import express from 'express';
+import cors from 'cors';
+import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import { HelloResolver } from './resolvers/Hello';
 import { connectDb } from './configs/connectDb';
 import { UserResolver } from './resolvers/users';
+import { MyContext } from './utils/types';
 
 dotenv.config();
 
 const bootstrap = async () => {
   try {
+    const app = express();
+
+    app.set('trust proxy', 1);
+    app.use(express.json());
+    app.use(
+      cors({
+        origin: 'http://localhost:3000',
+        credentials: true,
+      })
+    );
+
     const server = new ApolloServer({
       schema: await buildSchema({
         validate: false,
         resolvers: [HelloResolver, UserResolver],
       }),
+      context: ({ req, res }): MyContext => ({ req, res }),
+    });
+
+    server.applyMiddleware({
+      app,
+      cors: {
+        origin: 'http://localhost:3000',
+        credentials: true,
+      },
     });
 
     await connectDb();
-    server.listen().then(({ url }) => {
-      console.log(`🚀  Server ready at ${url}`);
+
+    app.listen({ port: 4000 }, () => {
+      console.log(
+        `🚀 Server ready at http://localhost:4000${server.graphqlPath}`
+      );
     });
   } catch (err) {
     console.log(err.message);
