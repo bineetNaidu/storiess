2dd057c1937a8f61fa081f9d71fb79d8c453d53a
bineetNaidu <bineet.s.naidu@gmail.com>
2021-06-06 13:54:31 +0530

    🚀 update UserResolver

--- packages/api/src/resolvers/users.ts
+++ packages/api/src/resolvers/users.ts
@@ -1,5 +1,14 @@
 import { User, UserModel } from '../models/User';
-import { Arg, Field, InputType, Mutation, Query, Resolver } from 'type-graphql';
+import {
+  Arg,
+  Ctx,
+  Field,
+  InputType,
+  Mutation,
+  Query,
+  Resolver,
+} from 'type-graphql';
+import { MyContext } from 'src/utils/types';
 
 @InputType()
 class UserInput {
@@ -9,19 +18,36 @@ class UserInput {
   email: string;
   @Field()
   googleId: string;
+  @Field()
+  avatar: string;
 }
 
 @Resolver()
 export class UserResolver {
   @Query(() => [User])
-  async users() {
+  async users(): Promise<User[]> {
     return UserModel.find({});
   }
 
   @Mutation(() => User)
-  async login(@Arg('input') input: UserInput) {
-    const user = await UserModel.create({ ...input });
-    user.save();
-    return user;
+  async login(
+    @Arg('input') input: UserInput,
+    @Ctx() { req }: MyContext
+  ): Promise<User> {
+    //? Find the user w/ google id
+    const existingUser = await UserModel.findOne({ googleId: input.googleId });
+    //? if there then return the existing user
+    if (existingUser) {
+      //? add user._id to session
+      req.session.userId = existingUser._id;
+      return existingUser;
+    } else {
+      //? else if not there then create the user
+      const newUser = await UserModel.create({ ...input });
+      newUser.save();
+      //? add user._id to session
+      req.session.userId = newUser._id;
+      return newUser;
+    }
   }
 }
