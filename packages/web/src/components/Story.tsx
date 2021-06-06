a5fa1307bde67ecd7256cf996b72b22029fb508a
bineetNaidu <bineet.s.naidu@gmail.com>
2021-06-06 23:03:54 +0530

    ❇️ UI / UX improvements

--- packages/web/src/components/Story.tsx
+++ packages/web/src/components/Story.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import { User } from '../generated/graphql';
 import { Avatar, WrapItem } from '@chakra-ui/react';
+import { motion } from 'framer-motion';
 
 interface Props {
   user: User;
@@ -8,21 +9,26 @@ interface Props {
 
 export const Story: FC<Props> = ({ user }) => {
   return (
-    <WrapItem
-      role="group"
-      border="2px"
-      p={0.5}
-      borderColor="linear(to-r, red.500, yellow.500)"
-      borderRadius="full"
-      cursor="pointer"
-      boxShadow="md"
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1, transition: { delay: 0.4 } }}
     >
-      <Avatar
-        _groupHover={{ transform: 'scale(1.2)' }}
-        name={user.username}
-        src={user.avatar!}
-        transition="all"
-      />
-    </WrapItem>
+      <WrapItem
+        role="group"
+        border="2px"
+        p={0.5}
+        borderColor="linear(to-r, red.500, yellow.500)"
+        borderRadius="full"
+        cursor="pointer"
+        boxShadow="md"
+      >
+        <Avatar
+          _groupHover={{ transform: 'scale(1.15)' }}
+          name={user.username}
+          src={user.avatar!}
+          transition="all"
+        />
+      </WrapItem>
+    </motion.div>
   );
 };
--- packages/web/src/pages/Home.tsx
+++ packages/web/src/pages/Home.tsx
@@ -1,15 +1,62 @@
-import { Container, Spinner, Wrap } from '@chakra-ui/react';
+import { AddIcon, BellIcon, ChatIcon } from '@chakra-ui/icons';
+import {
+  Box,
+  Container,
+  Flex,
+  Input,
+  Spinner,
+  Text,
+  Wrap,
+  IconButton,
+} from '@chakra-ui/react';
 import { useStoriesQuery } from 'src/generated/graphql';
 import { Story } from '../components/Story';
 
 export const Home = () => {
   const { data, loading } = useStoriesQuery();
   return (
-    <Container bgColor="#3c3f51" py={5}>
+    <Container bgColor="#3c3f51" py={5} rounded="md">
+      <Flex mb={5} justifyContent="center" alignItems="center">
+        <Input variant="outline" placeholder="@username" flex={0.4} />
+
+        <Flex justifyContent="flex-end" alignItems="center" flex={0.6}>
+          <IconButton aria-label="icon button">
+            <BellIcon />
+          </IconButton>
+          <IconButton aria-label="icon button" mx={2}>
+            <ChatIcon />
+          </IconButton>
+          <Box
+            as="button"
+            px={4}
+            py={2}
+            color="white"
+            fontWeight="bold"
+            borderRadius="md"
+            bgGradient="linear(to-r, red.500, yellow.500)"
+          >
+            <Flex alignItems="center">
+              <AddIcon mr={1} /> Add Photo
+            </Flex>
+          </Box>
+        </Flex>
+      </Flex>
+      <Text fontSize="2xl" fontWeight="bold">
+        Stories
+      </Text>
       {loading ? (
         <Spinner />
       ) : (
-        <Wrap>
+        <Wrap mt={4}>
+          {data && data.stories.map((u) => <Story key={u._id} user={u} />)}
+          {data && data.stories.map((u) => <Story key={u._id} user={u} />)}
+          {data && data.stories.map((u) => <Story key={u._id} user={u} />)}
+          {data && data.stories.map((u) => <Story key={u._id} user={u} />)}
+          {data && data.stories.map((u) => <Story key={u._id} user={u} />)}
+          {data && data.stories.map((u) => <Story key={u._id} user={u} />)}
+          {data && data.stories.map((u) => <Story key={u._id} user={u} />)}
+          {data && data.stories.map((u) => <Story key={u._id} user={u} />)}
+          {data && data.stories.map((u) => <Story key={u._id} user={u} />)}
           {data && data.stories.map((u) => <Story key={u._id} user={u} />)}
         </Wrap>
       )}
