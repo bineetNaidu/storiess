dda1c6ae4db13ee8132e342d84ca18170208ad93
bineetNaidu <bineet.s.naidu@gmail.com>
2021-06-06 12:01:38 +0530

    ❇️ add logic logic w/ error handling UI

--- packages/web/src/pages/Login.tsx
+++ packages/web/src/pages/Login.tsx
@@ -1,12 +1,42 @@
-import { Box, Text, Flex } from '@chakra-ui/react';
+import { Box, Text, Flex, useToast } from '@chakra-ui/react';
 import {
   GoogleLogin,
   GoogleLoginResponse,
   GoogleLoginResponseOffline,
 } from 'react-google-login';
+import { useLoginMutation } from '../generated/graphql';
 
 const Login = () => {
-  const responseGoogle = (res: GoogleLoginResponse) => {};
+  const [login] = useLoginMutation();
+  const toast = useToast();
+  const responseGoogle = async (res: GoogleLoginResponse) => {
+    try {
+      await login({
+        variables: {
+          input: {
+            email: res.profileObj.email,
+            googleId: res.googleId,
+            username: res.profileObj.name,
+          },
+        },
+      });
+    } catch (e) {
+      // alert(e.message);
+      if (
+        e.message.includes(
+          'E11000 duplicate key error collection: stories.users index: email_1 dup key:'
+        )
+      ) {
+        toast({
+          title: 'Error on Logging you in.',
+          description: 'Email is already in use.',
+          status: 'error',
+          duration: 9000,
+          isClosable: true,
+        });
+      }
+    }
+  };
 
   const errorResponse = (res: GoogleLoginResponseOffline) => {
     alert(res.code);
