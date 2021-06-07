989dc10c923d9cf4fb5f20b8edcb14546ffa6ae4
bineetNaidu <bineet.s.naidu@gmail.com>
2021-06-07 23:47:06 +0530

    ❇️ persist login session w/ google

--- packages/web/src/pages/Login.tsx
+++ packages/web/src/pages/Login.tsx
@@ -58,6 +58,7 @@ const Login = () => {
           clientId={process.env.REACT_APP_GOOGLE_CLIENT_KEY!}
           onSuccess={responseGoogle as any}
           onFailure={errorResponse}
+          isSignedIn={true}
         >
           <Box
             as="button"
