bf0b27b1811e49baf94fc5563662ee1ec7de1e5d
bineetNaidu <bineet.s.naidu@gmail.com>
2021-06-07 15:46:35 +0530

    🎉 add react-drop zone w/ cloudinary upload preset

--- packages/web/src/components/AddStoryModal.tsx
+++ packages/web/src/components/AddStoryModal.tsx
@@ -34,10 +34,38 @@ export const AddStoryModal: FC<Props> = ({ isOpen, onClose }) => {
           <Dropzone
             onDrop={async (files) => {
               try {
+                const url = `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUDINARY_CLOUD_NAME}/image/upload`;
+                const file = files[0];
+
+                const formData = new FormData();
+                formData.append('file', file);
+                formData.append(
+                  'upload_preset',
+                  process.env.REACT_APP_CLOUDINARY_PRESET_ID!
+                );
+
+                const res = await fetch(url, {
+                  method: 'POST',
+                  body: formData,
+                });
+
+                const {
+                  asset_id,
+                  etag,
+                  url: image_url,
+                  public_id,
+                  original_filename,
+                } = await res.json();
+
                 await addStory({
                   variables: {
-                    filename: files[0].name,
-                    image_url: files[0].name,
+                    input: {
+                      assetId: asset_id,
+                      etag,
+                      image_url,
+                      publicId: public_id,
+                      filename: original_filename,
+                    },
                   },
                 });
               } catch (e) {
