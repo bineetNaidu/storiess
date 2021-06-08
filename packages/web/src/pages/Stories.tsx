ea37e0c3bae6314272aeee538edaccdbae569930
bineetNaidu <bineet.s.naidu@gmail.com>
2021-06-08 11:32:14 +0530

    ✨ UI improvements

--- packages/web/src/pages/Stories.tsx
+++ packages/web/src/pages/Stories.tsx
@@ -1,22 +1,27 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 // import Swiper core and required modules
-import SwiperCore, { EffectCoverflow } from 'swiper/core';
-import { Avatar, Spinner } from '@chakra-ui/react';
+import SwiperCore, { EffectCoverflow, Autoplay } from 'swiper/core';
+import { Avatar, Spinner, IconButton } from '@chakra-ui/react';
 import { Box, Container, Flex, Text } from '@chakra-ui/layout';
 import { useParams, useHistory } from 'react-router-dom';
 import { useUserQuery } from '../generated/graphql';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
 // Import Swiper styles
 import 'swiper/swiper.min.css';
 import 'swiper/components/effect-coverflow/effect-coverflow.min.css';
+import { CloseIcon } from '@chakra-ui/icons';
 // import 'swiper/components/pagination/pagination.min.css';
 
 // install Swiper modules
-SwiperCore.use([EffectCoverflow]);
+SwiperCore.use([EffectCoverflow, Autoplay]);
 
 export const Stories = () => {
+  const [time, setTime] = useState('');
+  dayjs.extend(relativeTime);
   const history = useHistory();
   const { userId } = useParams<{ userId: string }>();
   const { loading, data } = useUserQuery({
@@ -35,16 +40,26 @@ export const Stories = () => {
         {loading ? (
           <Spinner />
         ) : (
-          <Box>
+          <Box position="relative">
             <Flex alignItems="center" mb={5}>
               <Avatar src={data?.user?.avatar!} mr={2} />
-              <Text>@{data?.user?.username}</Text>
+              <Text mr={2}>@{data?.user?.username}</Text>
+              <Text color="gray">{dayjs(time).fromNow()}</Text>
+
+              <IconButton
+                ml="auto"
+                onClick={() => history.push('/')}
+                aria-label="close button"
+                icon={<CloseIcon />}
+              />
             </Flex>
             <Swiper
               effect={'coverflow'}
               grabCursor={true}
               centeredSlides={true}
-              autoplay={true}
+              autoplay={{
+                delay: 2000,
+              }}
               slidesPerView={'auto'}
               coverflowEffect={{
                 rotate: 50,
@@ -55,9 +70,16 @@ export const Stories = () => {
               }}
             >
               {data?.user?.stories.map((story) => (
-                <SwiperSlide key={story._id}>
-                  <img src={story.image_url} alt={story.filename} />
-                </SwiperSlide>
+                <>
+                  <SwiperSlide key={story._id}>
+                    <img
+                      style={{ margin: '0 auto' }}
+                      src={story.image_url.replace('/upload', '/upload/h_500')}
+                      alt={story.filename}
+                      onFocus={() => setTime(story.createdAt)}
+                    />
+                  </SwiperSlide>
+                </>
               ))}
             </Swiper>
           </Box>
