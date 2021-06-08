cca14b900146c7496f2af025b2820a1f120416f2
bineetNaidu <bineet.s.naidu@gmail.com>
2021-06-08 21:55:51 +0530

    🎉 UI / UX improvement

--- packages/web/src/pages/Stories.tsx
+++ packages/web/src/pages/Stories.tsx
@@ -3,7 +3,7 @@ import relativeTime from 'dayjs/plugin/relativeTime';
 import SwiperCore, { EffectCoverflow, Autoplay } from 'swiper/core';
 import { useEffect, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Avatar, Spinner, IconButton } from '@chakra-ui/react';
+import { Avatar, Spinner, IconButton, useToast } from '@chakra-ui/react';
 import { Box, Container, Flex, Text } from '@chakra-ui/layout';
 import { CloseIcon, Icon, EditIcon } from '@chakra-ui/icons';
 import { useParams, useHistory } from 'react-router-dom';
@@ -24,6 +24,7 @@ import 'swiper/components/effect-coverflow/effect-coverflow.min.css';
 SwiperCore.use([EffectCoverflow, Autoplay]);
 
 export const Stories = () => {
+  const toast = useToast();
   const [time, setTime] = useState('');
   dayjs.extend(relativeTime);
   const history = useHistory();
@@ -84,23 +85,8 @@ export const Stories = () => {
                     alt={story.filename}
                     onFocus={() => setTime(story.createdAt)}
                     onDoubleClick={async () => {
-                      await likeStory({
-                        variables: {
-                          storyId: story._id,
-                        },
-                        update: (cache) => {
-                          cache.evict({ id: 'User:' + data.user?._id });
-                        },
-                      });
-                    }}
-                  />
-                  <Box mt={3}>
-                    <IconButton
-                      p={2}
-                      mx={1}
-                      aria-label="total likes"
-                      onClick={async () => {
-                        await removeLike({
+                      try {
+                        await likeStory({
                           variables: {
                             storyId: story._id,
                           },
@@ -108,6 +94,48 @@ export const Stories = () => {
                             cache.evict({ id: 'User:' + data.user?._id });
                           },
                         });
+                        toast({
+                          title: 'Like Added!',
+                          status: 'success',
+                          duration: 5000,
+                          isClosable: true,
+                        });
+                      } catch (e) {
+                        toast({
+                          title: 'Error Ocurred',
+                          description: e.message,
+                          status: 'error',
+                          duration: 5000,
+                          isClosable: true,
+                        });
+                      }
+                    }}
+                  />
+                  <Box mt={3}>
+                    <IconButton
+                      p={2}
+                      mx={1}
+                      aria-label="total likes"
+                      bgColor={story.likeStatus ? 'red.700' : undefined}
+                      onClick={async () => {
+                        try {
+                          await removeLike({
+                            variables: {
+                              storyId: story._id,
+                            },
+                            update: (cache) => {
+                              cache.evict({ id: 'User:' + data.user?._id });
+                            },
+                          });
+                        } catch (e) {
+                          toast({
+                            title: 'Error Ocurred',
+                            description: e.message,
+                            status: 'error',
+                            duration: 5000,
+                            isClosable: true,
+                          });
+                        }
                       }}
                       icon={
                         <>
