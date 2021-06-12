c3abc721b0a789961082970210c35259e460d2d4
bineetNaidu <bineet.s.naidu@gmail.com>
2021-06-12 23:09:58 +0530

    🎉 improve UX for new site users..

--- packages/web/src/pages/Home.tsx
+++ packages/web/src/pages/Home.tsx
@@ -1,13 +1,30 @@
-import { Container, Spinner, Text, Wrap } from '@chakra-ui/react';
+import { useEffect } from 'react';
+import {
+  Container,
+  Spinner,
+  Text,
+  Wrap,
+  useDisclosure,
+} from '@chakra-ui/react';
 import { useStoriesQuery } from 'src/generated/graphql';
 import { Story } from '../components/Story';
 import { Header } from '../components/Header';
+import { NewToSiteModal } from '../components/NewToSiteModal';
 
 export const Home = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const { data, loading, refetch } = useStoriesQuery();
+
+  useEffect(() => {
+    const nts = localStorage.getItem('stories:nts');
+    if (nts === 'true') {
+      onOpen();
+    }
+  }, [onOpen]);
   return (
     <Container bgColor="#3c3f51" py={5} rounded="md">
       <Header refetch={refetch} />
+      <NewToSiteModal onClose={onClose} isOpen={isOpen} />
       <Text fontSize="2xl" fontWeight="bold">
         Stories
       </Text>
