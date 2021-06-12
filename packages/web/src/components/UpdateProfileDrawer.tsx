193de72cf5efe5a86ccf1c3644afa38044076f14
bineetNaidu <bineet.s.naidu@gmail.com>
2021-06-12 14:43:01 +0530

    ✨ add update Profile UI w/ mutations

---
+++ packages/web/src/components/UpdateProfileDrawer.tsx
@@ -0,0 +1,102 @@
+import { FC, MutableRefObject } from 'react';
+import {
+  Box,
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  FormLabel,
+  Stack,
+  useToast,
+} from '@chakra-ui/react';
+import { Textarea } from '@chakra-ui/textarea';
+import { Formik, Form } from 'formik';
+import { useUpdateUserMutation, Maybe } from 'src/generated/graphql';
+
+interface Props {
+  drawerRef: MutableRefObject<any>;
+  isOpen: boolean;
+  onClose: () => void;
+  initialValue: Maybe<string> | undefined;
+}
+
+export const UpdateProfileDrawer: FC<Props> = ({
+  drawerRef,
+  isOpen,
+  onClose,
+  initialValue,
+}) => {
+  const toast = useToast();
+  const [updateUser] = useUpdateUserMutation();
+  return (
+    <Drawer
+      isOpen={isOpen}
+      placement="right"
+      initialFocusRef={drawerRef}
+      onClose={onClose}
+    >
+      <DrawerOverlay />
+      <Formik
+        initialValues={{ bio: initialValue || '' }}
+        onSubmit={async (value) => {
+          updateUser({
+            variables: value,
+            update: (cache, data) => {
+              cache.evict({ id: 'User:' + data.data?.updateUser._id });
+            },
+          });
+          toast({
+            title: 'Updated!',
+            description: 'You Have successfully update your bio',
+            duration: 5000,
+            isClosable: true,
+            status: 'info',
+          });
+          onClose();
+        }}
+      >
+        {({ isSubmitting, getFieldProps }) => (
+          <Form>
+            <DrawerContent>
+              <DrawerCloseButton />
+              <DrawerHeader borderBottomWidth="1px">
+                Update Profile
+              </DrawerHeader>
+
+              <DrawerBody>
+                <Stack spacing="24px">
+                  <Box>
+                    <FormLabel htmlFor="bio">Bio</FormLabel>
+                    <Textarea
+                      type="textarea"
+                      id="bio"
+                      autoFocus
+                      {...getFieldProps('bio')}
+                    />
+                  </Box>
+                </Stack>
+              </DrawerBody>
+
+              <DrawerFooter borderTopWidth="1px">
+                <Button variant="outline" mr={3} onClick={onClose}>
+                  Cancel
+                </Button>
+                <Button
+                  isLoading={isSubmitting}
+                  colorScheme="blue"
+                  type="submit"
+                >
+                  Submit
+                </Button>
+              </DrawerFooter>
+            </DrawerContent>
+          </Form>
+        )}
+      </Formik>
+    </Drawer>
+  );
+};
--- packages/web/src/pages/Profile.tsx
+++ packages/web/src/pages/Profile.tsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -12,30 +13,43 @@ import {
   IconButton,
   Tooltip,
   useToast,
+  useDisclosure,
+  Button,
 } from '@chakra-ui/react';
-import { useReportUserMutation, useUserQuery } from 'src/generated/graphql';
+import {
+  useMeQuery,
+  useReportUserMutation,
+  useUserQuery,
+} from 'src/generated/graphql';
 import { useParams } from 'react-router';
 import { WarningIcon } from '@chakra-ui/icons';
+import { Link } from 'react-router-dom';
+import { UpdateProfileDrawer } from '../components/UpdateProfileDrawer';
 
 export const Profile = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const drawerRef = useRef<any>();
   const toast = useToast();
   const { userId } = useParams<{ userId: string }>();
   const { loading, data } = useUserQuery({
     variables: { id: userId },
   });
   const [reportUser] = useReportUserMutation();
+  const { data: meData } = useMeQuery();
 
   return (
     <Container bgColor="#3c3f51" py={5} rounded="md">
       <Breadcrumb>
         <BreadcrumbItem>
-          <BreadcrumbLink href="/">/ dashboard</BreadcrumbLink>
+          <BreadcrumbLink as={Link} to="/">
+            dashboard
+          </BreadcrumbLink>
         </BreadcrumbItem>
         <BreadcrumbItem>
           <BreadcrumbLink>user</BreadcrumbLink>
         </BreadcrumbItem>
         <BreadcrumbItem isCurrentPage>
-          <BreadcrumbLink href={`/u/${data?.user?._id}`}>
+          <BreadcrumbLink as={Link} to={`/u/${data?.user?._id}`}>
             {data?.user?.username}
           </BreadcrumbLink>
         </BreadcrumbItem>
@@ -62,32 +76,48 @@ export const Profile = () => {
                 <Box>
                   <Flex alignItems="center" mb={3}>
                     <Text mr={4}>@{data.user.username}</Text>
-                    <Tooltip hasArrow label="Click Report User" bg="blue.200">
-                      <IconButton
-                        size="xs"
-                        aria-label="report user button"
-                        icon={<WarningIcon />}
-                        onClick={async () => {
-                          if (data.user) {
-                            const { data: reportData } = await reportUser({
-                              variables: { userId: data.user._id! },
-                            });
-                            if (reportData?.reportUser !== null) {
-                              toast({
-                                title: 'Reported',
-                                description: `You Have successfully reported against ${data.user.username}`,
-                                isClosable: true,
-                                duration: 5000,
-                                status: 'info',
+                    {meData?.me?._id !== data.user._id ? (
+                      <Tooltip hasArrow label="Click Report User" bg="blue.200">
+                        <IconButton
+                          size="xs"
+                          aria-label="report user button"
+                          icon={<WarningIcon />}
+                          onClick={async () => {
+                            if (data.user) {
+                              const { data: reportData } = await reportUser({
+                                variables: { userId: data.user._id! },
                               });
+                              if (reportData?.reportUser !== null) {
+                                toast({
+                                  title: 'Reported',
+                                  description: `You Have successfully reported against ${data.user.username}`,
+                                  isClosable: true,
+                                  duration: 5000,
+                                  status: 'info',
+                                });
+                              }
                             }
-                          }
-                        }}
-                      />
-                    </Tooltip>
+                          }}
+                        />
+                      </Tooltip>
+                    ) : (
+                      <>
+                        <Button colorScheme="teal" onClick={onOpen} size="xs">
+                          Update Profile
+                        </Button>
+                        <UpdateProfileDrawer
+                          onClose={onClose}
+                          drawerRef={drawerRef}
+                          isOpen={isOpen}
+                          initialValue={data.user.bio}
+                        />
+                      </>
+                    )}
                   </Flex>
                   {data.user.bio ? (
-                    <Text mt={1}>{data.user.bio}</Text>
+                    <Text mt={1} color="gray.300">
+                      {data.user.bio}
+                    </Text>
                   ) : (
                     <Text mt={1} color="gray" fontStyle="italic">
                       {data.user.username} has setup their bio
