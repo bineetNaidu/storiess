2a0604fa00c0de470e2837f5cfa4bcc5f52ad99b
Bineet Naidu <bineet.s.naidu@gmail.com>
2023-01-03 23:18:23 +0530

    🏗️ remove apollo client

--- web_v2/lib/apollo-client.ts
+++
@@ -1,7 +0,0 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-
-export const client = new ApolloClient({
-  uri: process.env.API_URL!,
-  cache: new InMemoryCache(),
-  credentials: 'include',
-});
--- web_v2/pages/_app.tsx
+++ web_v2/pages/_app.tsx
@@ -1,15 +1,11 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
-import { ApolloProvider } from '@apollo/client';
-import { client } from '../lib/apollo-client';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider resetCSS>
-      <ApolloProvider client={client}>
-        <Component {...pageProps} />
-      </ApolloProvider>
+      <Component {...pageProps} />
     </ChakraProvider>
   );
 }
