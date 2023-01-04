20e477c9d355bf5f1060a0d086d4a9ea39284eff
Bineet Naidu <bineet.s.naidu@gmail.com>
2023-01-04 17:07:18 +0530

    🚀 add React Query Provider

--- web_v2/pages/_app.tsx
+++ web_v2/pages/_app.tsx
@@ -1,11 +1,16 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+
+const client = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider resetCSS>
-      <Component {...pageProps} />
+      <QueryClientProvider client={client}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
     </ChakraProvider>
   );
 }
